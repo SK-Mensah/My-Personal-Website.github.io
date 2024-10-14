5b81a30a620ce4746dffe7db623956d3ec17aa84
Samuel Mensah <sam12mensah@gmail.com>
2024-10-14 17:59:54 +0200

    hero updates

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-//typing text
-const text = "Hello, welcome to dynamic typing!";
-const typingTextElement = document.getElementById('typing-text');
-let index = 0;
 
-function type() {
-    if (index < text.length) {
-        typingTextElement.textContent += text.charAt(index);
-        index++;
-        setTimeout(type, 100); // Adjust typing speed here (in milliseconds)
-    }
-}
-
-type();
