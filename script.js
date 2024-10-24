a5e4977154c6a153bb9cbc82919ee08b098e18ef
Samuel Mensah <sam12mensah@gmail.com>
2024-10-24 16:51:59 +0200

    top nav

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+//TOP NAVIGATION
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive"; // Add responsive class
+    } else {
+        x.className = "topnav"; // Remove responsive class
+    }
+}
+
+
+
+
 
