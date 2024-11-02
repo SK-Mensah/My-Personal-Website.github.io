0f105c82cf102e2af1dfb5e32c108417b55fb898
Samuel Mensah <s223130680@M104-35>
2024-11-03 01:48:50 +0200

    .

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-//TOP NAVIGATION
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive"; // Add responsive class
-    } else {
-        x.className = "topnav"; // Remove responsive class
-    }
-}
+
 
 
 
